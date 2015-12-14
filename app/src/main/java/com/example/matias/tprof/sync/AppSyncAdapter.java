1d8a459e4dd2f8ba9f45f83cc685bcd878128a36
mdeantoni <mdeantoni87@gmail.com>
2015-12-14 00:00:18 -0300

    Logic for updating tables with json result

--- app/src/main/java/com/example/matias/tprof/sync/AppSyncAdapter.java
+++ app/src/main/java/com/example/matias/tprof/sync/AppSyncAdapter.java
@@ -5,14 +5,30 @@ import android.accounts.AccountManager;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SyncRequest;
 import android.content.SyncResult;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.matias.tprof.R;
+import com.example.matias.tprof.data.QuotesContract;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Vector;
 
 /**
  * Created by Mati on 12/13/2015.
@@ -22,7 +38,7 @@ public class AppSyncAdapter extends AbstractThreadedSyncAdapter {
     // Interval at which to sync with the weather, in seconds.
     // 60 seconds (1 minute) * 180 = 3 hours
     public static final int SYNC_INTERVAL = 60 * 180;
-    public static final int SYNC_FLEXTIME = SYNC_INTERVAL/3;
+    public static final int SYNC_FLEXTIME = SYNC_INTERVAL / 3;
 
     public AppSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
@@ -30,14 +46,181 @@ public class AppSyncAdapter extends AbstractThreadedSyncAdapter {
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-        Log.d(LOG_TAG,"Syncing!!!!!!!!!!!!!!!!!!!!!!!!");
-        Log.d(LOG_TAG,"Syncing!!!!!!!!!!!!!!!!!!!!!!!!");
-        Log.d(LOG_TAG,"Syncing!!!!!!!!!!!!!!!!!!!!!!!!");
-        Log.d(LOG_TAG,"Syncing!!!!!!!!!!!!!!!!!!!!!!!!");
+        Log.d(LOG_TAG, "Starting Sync");
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String testData = null;
+
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are avaiable at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            final String BASE_URL =
+                 //   "http://10.0.2.2:50914/api/sync";
+             "http://192.168.0.17:50914/api/sync";
+
+            Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            int code = urlConnection.getResponseCode();
+            String resp = urlConnection.getResponseMessage();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return;
+            }
+            testData = buffer.toString();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attemping
+            // to parse it.
+            return;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+
+        try { //OJO CON ESTO QUE EN EL EJEMPLO ESTA AL REVES EL ORDEN CON EL CIERRE DE CONNECTION
+            getQuotesFromJson(testData);
+        } catch (JSONException e) {
+            Log.e("Some log", e.getMessage(), e);
+            e.printStackTrace();
+        }
+        Log.d(LOG_TAG, "Finished Sync");
+        return;
+    }
+
+    public void getQuotesFromJson(String syncData) throws JSONException {
+        JSONObject syncDataObject = new JSONObject(syncData);
+        JSONObject dataObject = syncDataObject.getJSONObject("Data");
+        JSONArray stocksArray = dataObject.getJSONArray("Stocks");
+        JSONArray bondsArray = dataObject.getJSONArray("Bonds");
+        this.getStockQuotesFromJson(stocksArray);
+        this.getBondQuotesFromJson(bondsArray);
+    }
+
+    private void getStockQuotesFromJson(JSONArray quotesArray) throws JSONException {
+        final String QUOTE_NAME = "FullName";
+        final String SYMBOL_NAME = "TickerSymbol";
+
+        Vector<ContentValues> cVVector = new Vector<ContentValues>(quotesArray.length());
+
+        for (int i = 0; i < quotesArray.length(); i++) {
+            String symbol;
+            String fullName;
+
+            JSONObject quote = quotesArray.getJSONObject(i);
+            fullName = quote.getString(QUOTE_NAME);
+            symbol = quote.getString(SYMBOL_NAME);
+
+            ContentValues weatherValues = new ContentValues();
+
+            weatherValues.put(QuotesContract.StockEntry.COLUMN_SYMBOL, symbol);
+            weatherValues.put(QuotesContract.StockEntry.COLUMN_FULLNAME, fullName);
+            cVVector.add(weatherValues);
+        }
+
+        Cursor stockCursor = getContext().getContentResolver().query(
+                QuotesContract.StockEntry.CONTENT_URI,
+                new String[]{QuotesContract.StockEntry._ID},
+                null,
+                null,
+                null);
+
+        if (!stockCursor.moveToFirst()) {
+            int inserted = 0;
+            // add to database if there werent any stocks!
+            if (cVVector.size() > 0) {
+                ContentValues[] cvArray = new ContentValues[cVVector.size()];
+                cVVector.toArray(cvArray);
+                inserted = getContext().getContentResolver().bulkInsert(QuotesContract.StockEntry.CONTENT_URI, cvArray);
+            }
+
+            Log.d(LOG_TAG, "FetchStockDataTask Complete. " + inserted + " Inserted");
+        }
+    }
+
+    private void getBondQuotesFromJson(JSONArray quotesArray) throws JSONException {
+        final String QUOTE_NAME = "FullName";
+        final String SYMBOL_NAME = "TickerSymbol";
+
+        Vector<ContentValues> cVVector = new Vector<ContentValues>(quotesArray.length());
+
+        for (int i = 0; i < quotesArray.length(); i++) {
+            String symbol;
+            String fullName;
+
+            JSONObject quote = quotesArray.getJSONObject(i);
+            fullName = quote.getString(QUOTE_NAME);
+            symbol = quote.getString(SYMBOL_NAME);
+
+            ContentValues quoteValues = new ContentValues();
+
+            quoteValues.put(QuotesContract.BondEntry.COLUMN_SYMBOL, symbol);
+            quoteValues.put(QuotesContract.BondEntry.COLUMN_FULLNAME, fullName);
+            cVVector.add(quoteValues);
+        }
+
+        Cursor bondCursor = getContext().getContentResolver().query(
+                QuotesContract.BondEntry.CONTENT_URI,
+                new String[]{QuotesContract.BondEntry._ID},
+                null,
+                null,
+                null);
+
+        if (!bondCursor.moveToFirst()) {
+            int inserted = 0;
+            // add to database if there werent any stocks!
+            if (cVVector.size() > 0) {
+                ContentValues[] cvArray = new ContentValues[cVVector.size()];
+                cVVector.toArray(cvArray);
+                inserted = getContext().getContentResolver().bulkInsert(QuotesContract.BondEntry.CONTENT_URI, cvArray);
+            }
+
+            Log.d(LOG_TAG, "FetchBondDataTask Complete. " + inserted + " Inserted");
+        }
     }
 
     /**
      * Helper method to have the sync adapter sync immediately
+     *
      * @param context The context used to access the account service
      */
     public static void syncImmediately(Context context) {
@@ -66,7 +249,7 @@ public class AppSyncAdapter extends AbstractThreadedSyncAdapter {
                 context.getString(R.string.app_name), context.getString(R.string.sync_account_type));
 
         // If the password doesn't exist, the account doesn't exist
-        if ( null == accountManager.getPassword(newAccount) ) {
+        if (null == accountManager.getPassword(newAccount)) {
 
         /*
          * Add the account and account type, no password or user data
@@ -91,7 +274,7 @@ public class AppSyncAdapter extends AbstractThreadedSyncAdapter {
         /*
          * Since we've created an account
          */
-       // AppSyncAdapter.configurePeriodicSync(context, SYNC_INTERVAL, SYNC_FLEXTIME);
+        // AppSyncAdapter.configurePeriodicSync(context, SYNC_INTERVAL, SYNC_FLEXTIME);
 
         /*
          * Without calling setSyncAutomatically, our periodic sync will not be enabled.
