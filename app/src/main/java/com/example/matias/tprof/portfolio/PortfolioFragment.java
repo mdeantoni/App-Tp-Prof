ef78c1e40fd927907ac32f0c7542844a98df5f2b
mdeantoni <mdeantoni87@gmail.com>
2016-07-15 00:38:38 -0300

    Added chart for detail of portfolio

--- app/src/main/java/com/example/matias/tprof/portfolio/PortfolioFragment.java
+++ app/src/main/java/com/example/matias/tprof/portfolio/PortfolioFragment.java
@@ -7,9 +7,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.matias.tprof.R;
@@ -84,28 +88,68 @@ public class PortfolioFragment extends Fragment  implements LoaderManager.Loader
         PieChart pieChart = (PieChart) getView().findViewById(R.id.portfolio_chart);
         ArrayList<Entry> entries = new ArrayList<>();
         ArrayList<String> labels = new ArrayList<String>();
+        ArrayList<TableRow> detailRows = new ArrayList<TableRow>();
 
         while (cursor.moveToNext()) {
             float currentValue = cursor.getFloat(V_HOLDING_TOTAL_VALUE);
             totalHoldings += currentValue;
             entries.add(new Entry(currentValue, cursor.getPosition()));
             labels.add(cursor.getString(V_HOLD_COL_SYMBOL));
+
+
+            TableRow newRow = new TableRow(getActivity());
+            TextView symbolTv = new TextView(getActivity());
+            TextView holdingTv =  new TextView(getActivity());
+            TextView lastPriceTv =  new TextView(getActivity());
+            TextView totalValuedTv =  new TextView(getActivity());
+
+            symbolTv.setText(cursor.getString(V_HOLD_COL_SYMBOL));
+            holdingTv.setText(cursor.getString(V_HOLDING_COL_QTY));
+            lastPriceTv.setText("$" + String.format("%.2f", cursor.getFloat(V_HOLD_COL_CURRENT_PRICE)) );
+            totalValuedTv.setText("$" + String.format("%.2f", cursor.getFloat(V_HOLDING_TOTAL_VALUE)));
+
+            symbolTv.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
+            symbolTv.setGravity(Gravity.LEFT);
+            symbolTv.setTextAppearance(getActivity(), android.R.style.TextAppearance_Material_Medium);
+            holdingTv.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
+            holdingTv.setGravity(Gravity.RIGHT);
+            holdingTv.setTextAppearance(getActivity(), android.R.style.TextAppearance_Material_Medium);
+            lastPriceTv.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
+            lastPriceTv.setGravity(Gravity.RIGHT);
+            lastPriceTv.setTextAppearance(getActivity(), android.R.style.TextAppearance_Material_Medium);
+            totalValuedTv.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
+            totalValuedTv.setGravity(Gravity.RIGHT);
+            totalValuedTv.setTextAppearance(getActivity(), android.R.style.TextAppearance_Material_Medium);
+
+            newRow.addView(symbolTv);
+            newRow.addView(holdingTv);
+            newRow.addView(lastPriceTv);
+            newRow.addView(totalValuedTv);
+
+            detailRows.add(newRow);
         }
 
         PieDataSet dataset = new PieDataSet(entries, "");
         dataset.setColors(ColorTemplate.PASTEL_COLORS);
         PieData data = new PieData(labels, dataset); // initialize Piedata
-        data.setValueTextSize(10f);
+        data.setValueTextSize(12f);
         data.setValueFormatter(new PercentFormatter());
         pieChart.setUsePercentValues(true);
+        pieChart.setDrawSliceText(false);
         pieChart.setHoleRadius(20f);
         pieChart.setTransparentCircleRadius(20f);
         pieChart.setData(data); //set data into chart
         pieChart.setDescription(" ");  // set the description
         pieChart.setTouchEnabled(false);
+        pieChart.getLegend().setTextSize(12f);
 
         TextView totalHoldingsTV = (TextView) getView().findViewById(R.id.portfolio_total);
         totalHoldingsTV.setText("$ "+ String.format("%.2f", totalHoldings));
+
+        TableLayout tableLayout = (TableLayout) getView().findViewById(R.id.detail_table);
+        for (TableRow row: detailRows) {
+            tableLayout.addView(row);
+        }
     }
 
     @Override
