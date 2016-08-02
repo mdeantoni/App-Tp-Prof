18bc4fb4c43ae2e7c93695dc8679213ee0883e91
mdeantoni <mdeantoni87@gmail.com>
2016-08-02 00:29:36 -0300

    Fragmento y tabs de operaciones

--- app/src/main/java/com/example/matias/tprof/portfolio/PortfolioActivity.java
+++ app/src/main/java/com/example/matias/tprof/portfolio/PortfolioActivity.java
@@ -1,13 +1,19 @@
 package com.example.matias.tprof.portfolio;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.example.matias.tprof.R;
+import com.example.matias.tprof.ViewPagerAdapter;
 
 public class PortfolioActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,14 @@ public class PortfolioActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
        // getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager_portfolio);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new PortfolioFragment(), "Portfolio");
+        adapter.addFragment(new TradesFragment(), "Operaciones");
+        viewPager.setAdapter(adapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs_portfolio);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
